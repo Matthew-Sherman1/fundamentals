73d6dfb8f2b357680069fe0509d02edcf935632f
Matthew Sherman <matt5064@yahoo.com>
2021-11-29 14:44:01 -0700

    Inital commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+my_name = "Matthew"
+print('Hello my name is', my_name, "!")
+print('Hello my name is ' + my_name +"!")
+my_number = str(25)
+print('Hello', my_number, "!")
+print('Hello ' + my_number +"!")
+food_one = "pizza"
+food_two = "steak"
+print("I love to eat {} and {}.".format(food_one, food_two))
+print(f"I love to eat {food_one} and {food_two}.")
+
+

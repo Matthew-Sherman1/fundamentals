595202581e009706dd1f0d70c1c74a1bf37c3b25
Matthew Sherman <matt5064@yahoo.com>
2021-11-30 00:30:28 -0700

    initial commit

---
+++ functions_intermediate_1.py
@@ -0,0 +1,75 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+print(x)
+students[0]['last_name'] = 'Bryant'
+print(students)
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'])
+z[0]['y']=30
+print(z)
+#----------------------------------------------------------------------------------------------------------------------
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(students):
+    for i in range(len(students)):
+        print("first_name - "+students[i]['first_name']+", last_name - "+students[i]['last_name'])
+iterateDictionary(students)
+#----------------------------------------------------------------------------------------------------------------------
+def iterateDictionary(some_list):
+    for index in range(len(some_list)):
+        for key in some_list[index]:
+            print(some_list[index][key])
+iterateDictionary([
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ])
+#----------------------------------------------------------------------------------------------------------------------
+def printInfo(some_dict):
+    for key in some_dict:
+        print(len(some_dict[key]),key)
+        for i in range(len(some_dict[key])):
+            print(some_dict[key][i])
+        print("")
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+#----------------------------------------------------------------------------------------------------------------------
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+
+
